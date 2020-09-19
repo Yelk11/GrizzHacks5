6630fea3b9325d0e88b20e9413869a7d86eae345
Yelk11 <neighbour.matthew@gmail.com>
2020-09-19 01:00:35 -0400

    Idk

---
+++ MuscleStressSimulation/src/main/java/com/grizzhacks/musclestresssimulation/muscles/Muscle.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grizzhacks.musclestresssimulation.muscles;
+
+/**
+ *
+ * @author Matt
+ */
+public class Muscle {
+    
+}
